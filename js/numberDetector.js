2a311f3f114aa8d5939c4297cb19b982854b76eb
vincent <vincent.gbs11@gmail.com>
2019-05-09 09:28:01 -0400

    Working to add number images to image detection

--- js/numberDetector.js
+++ js/numberDetector.js
@@ -1,12 +1,24 @@
-const trainingData = null;
+// detecting numbers in "images"
 
 const net = new brain.NeuralNetwork();
+const trainingData = [
+    { input: zero, output: { zero: 1 } },
+    { input: one, output: { one: 1 } },
+    { input: two, output: { two: 1 } },
+    { input: three, output: { three: 1 } },
+    { input: four, output: { four: 1 } },
+    { input: five, output: { five: 1 } },
+    { input: six, output: { six: 1 } },
+    { input: seven, output: { seven: 1 } },
+    { input: eight, output: { eight: 1 } },
+    { input: nine, output: { nine: 1 } }
+];
 
 globalVar = {
     train: function() {
-        net.train(trainingData);
+        net.train(trainingData, { log: (stats) => console.log(stats) });
     },
     run: function() {
-        console.log(net.run('example'));
+        // console.log(net.run('example'));
     }
 };
---
+++ js/numberImages.js
@@ -0,0 +1,98 @@
+function toArray(string) { // normalize
+    if (string.length !== 7 * 7) throw new Error('string in wrong size');
+    return string.split('').map(toNumber);
+}
+function toNumber(character) {
+    return character === '#' ? 1 : 0;
+}
+
+const zero = toArray(
+    '#######' +
+    '#     #' +
+    '#     #' +
+    '#     #' +
+    '#     #' +
+    '#     #' +
+    '#######'
+);
+const one = toArray(
+    '   #   ' +
+    '   #   ' +
+    '   #   ' +
+    '   #   ' +
+    '   #   ' +
+    '   #   ' +
+    '   #   '
+);
+const two = toArray(
+    '#######' +
+    '#     #' +
+    '      #' +
+    '     # ' +
+    '   #   ' +
+    ' #     ' +
+    '#######'
+);
+  const three = toArray(
+    '#######' +
+    '      #' +
+    '      #' +
+    ' ######' +
+    '      #' +
+    '      #' +
+    '#######'
+);
+const four = toArray(
+    '#     #' +
+    '#     #' +
+    '#     #' +
+    '#######' +
+    '      #' +
+    '      #' +
+    '      #'
+);
+const five = toArray(
+    '#######' +
+    '#      ' +
+    '#      ' +
+    '#######' +
+    '      #' +
+    '      #' +
+    '#######'
+);
+const six = toArray(
+    '      #' +
+    '    #  ' +
+    '  #    ' +
+    ' ######' +
+    '#     #' +
+    '#     #' +
+    '#######'
+);
+const seven = toArray(
+    '#######' +
+    '     # ' +
+    '    #  ' +
+    '   #   ' +
+    '  #    ' +
+    ' #     ' +
+    '#      '
+);
+const eight = toArray(
+    '#######' +
+    '#     #' +
+    '#     #' +
+    '#######' +
+    '#     #' +
+    '#     #' +
+    '#######'
+);
+const nine = toArray(
+    '#######' +
+    '#     #' +
+    '#     #' +
+    '###### ' +
+    '    #  ' +
+    '   #   ' +
+    ' #     '
+);
