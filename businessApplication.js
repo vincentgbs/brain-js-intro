554c4310bd95f595828fa1bb79ce6dc3092b92f5
vincent <vincent.gbs11@gmail.com>
2019-05-10 09:08:35 -0400

    Added promises to load lexicons and train neural net, training on such a big data set takes a long time, but smaller iterations gives quite poor results

--- businessApplication.js
+++ businessApplication.js
@@ -1,8 +1,8 @@
-const trainingData = [
-    { input: 'I am super happy!', output: 'happy' },
-    { input: 'What a pill!', output: 'sarcastic' },
-    { input: 'I am super unhappy!', output: 'sad' },
-    { input: 'Are we there yet?', output: 'excited' }
+let trainingData = [
+    // { input: 'I am super happy!', output: 'happy' },
+    // { input: 'What a pill!', output: 'sarcastic' },
+    // { input: 'I am super unhappy!', output: 'sad' },
+    // { input: 'Are we there yet?', output: 'excited' }
 ];
 
 const net = new brain.recurrent.LSTM();
@@ -15,8 +15,8 @@ globalVar = {
     train: function() {
         let promise = new Promise( (resolve, reject) => {
             let done = net.train(trainingData, {
-                iterations: 500,
-                errorThresh: 0.011
+                iterations: 999,
+                errorThresh: 0.1
             });
             if (done) {
                 resolve();
@@ -26,7 +26,7 @@ globalVar = {
         });
 
         promise.then( () => {
-            console.log('Training done');
+            console.log('Training done.');
             document.getElementById("run").removeAttribute("disabled");
         }).catch( () => {
             console.log('Neural net did not train!');
@@ -35,5 +35,27 @@ globalVar = {
     run: function() {
         const input = document.getElementById('user_input').value;
         logToWindowConsole(input +' -> ' + net.run(input));
+    },
+    loadLexicons: function() {
+        let promise = new Promise( (resolve, reject) => {
+            for (i = 0; i < negative.length; i++) {
+                trainingData.push({input: negative[i], output:'negative'});
+            }
+            for (i = 0; i < positive.length; i++) {
+                trainingData.push({input: positive[i], output:'positive'});
+            }
+            if (true) {
+                resolve();
+            } else {
+                reject();
+            }
+        });
+
+        promise.then( () => {
+            console.log('Lexicons loaded.');
+            globalVar.train();
+        }).catch( () => {
+            console.log('Error loading lexicons.');
+        });
     }
 };
